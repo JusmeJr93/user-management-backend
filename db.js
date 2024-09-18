0d3daebacd18faf546da5af47e53d0feae29ca44
Junior Jusme <junior.jusme@gmail.com>
2024-09-17 21:09:58 -0600

    heroku env

--- db.js
+++ db.js
@@ -3,9 +3,18 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).promise();
+let connectionConfig;
+
+if (process.env.JAWSDB_URL) {
+    // Heroku environment
+    connectionConfig = process.env.JAWSDB_URL;
+} else {
+    connectionConfig = {
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE
+    };
+}
+
+export const pool = mysql.createPool(connectionConfig).promise();
